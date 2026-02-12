6c09eff2e96ad6e2a74d79c9815a933766469a4f
joshhellawell <josh@hellawell.co.uk>
2026-02-12 16:58:43 +0000

    Added NextJS Suspense wrapper to avoid Router errors

---
+++ src/app/(auth)/login/LoginForm.tsx
@@ -0,0 +1,88 @@
+'use client'
+
+import Link from 'next/link'
+
+import { Button } from '@/components/Button'
+import { TextField } from '@/components/Fields'
+import { Logo } from '@/components/Logo'
+import { SlimLayout } from '@/components/SlimLayout'
+import { useState } from 'react'
+import { useSearchParams, useRouter } from 'next/navigation'
+
+export default function LoginForm() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const emailFromUrl = searchParams.get('email') || ''
+
+  const [email, setEmail] = useState(emailFromUrl)
+  const [password, setPassword] = useState('')
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const res = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+
+    if (res.ok) {
+      router.push('/dashboard')
+    } else {
+      alert('Login failed')
+    }
+  }
+
+  return (
+    <SlimLayout>
+      <div className="flex">
+        <Link href="/" aria-label="Home">
+          <Logo className="h-10 w-auto" />
+        </Link>
+      </div>
+      <h2 className="mt-20 text-lg font-semibold text-gray-900">
+        Sign in to your account
+      </h2>
+      <p className="mt-2 text-sm text-gray-700">
+        Don’t have an account?{' '}
+        <Link
+          href="/register"
+          className="font-medium text-blue-600 hover:underline"
+        >
+          Sign up
+        </Link>{' '}
+        for a free trial.
+      </p>
+      <form onSubmit={handleSubmit} className="mt-10 grid grid-cols-1 gap-y-8">
+        <TextField
+          label="Email address"
+          name="email"
+          type="email"
+          autoComplete="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <TextField
+          label="Password"
+          name="password"
+          type="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <div>
+          <Button type="submit" variant="solid" color="blue" className="w-full">
+            <span>
+              Sign in <span aria-hidden="true">&rarr;</span>
+            </span>
+          </Button>
+        </div>
+      </form>
+    </SlimLayout>
+  )
+}
