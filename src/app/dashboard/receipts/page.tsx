db5af44ff487fcad996e7eb20d91762a2ae716e0
joshhellawell <josh@hellawell.co.uk>
2026-03-07 13:45:31 +0000

    Fixing linting errors

--- src/app/dashboard/receipts/page.tsx
+++ src/app/dashboard/receipts/page.tsx
@@ -1,6 +1,9 @@
 import ReceiptsPage from '@/containers/ReceiptsPage/ReceiptsPage'
 import { GetAllReceipts } from '@/lib/server/receipts/getAllReceipts'
 
+// This forces the build to run dynamically, rather than requiring an active endpoint
+export const dynamic = 'force-dynamic'
+
 export default async function Receipts() {
   const result = await GetAllReceipts()
   if (!result.ok) throw new Error(result.message)
